95707cd45dd36d897e8cd6897ff1c58f0e006f2a
brizzeta <nikolmotorina@gmail.com>
2024-03-21 14:19:51 +0200

    1. 2 Part

---
+++ project2/src/app/articles.ts
@@ -0,0 +1,17 @@
+export const articles = [
+    {
+        src: 'assets/img/1.jpg',
+        title: ' Free Entertaimment',
+        text: ' Our advanced, touch-screen digital entertainment system puts you in charge of your entertainment options. '
+    },
+    {
+        src: 'assets/img/2.jpg',
+        title: 'Turn Dreamming',
+        text: 'Customize your ideal vacation to 300+ destinations worldwide with Delta Vacations. ',
+    },
+    {
+        src: 'assets/img/3.jpg',
+        title: 'Gift Card',
+        text: 'Give the Gift of Go™ and let them explore more than 300 destinations worldwide. Terms apply. Shop now. ',
+    }
+];

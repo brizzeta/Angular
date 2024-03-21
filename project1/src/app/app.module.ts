cfb37c7e8a017c93572176410e3b485edd6b00b7
brizzeta <nikolmotorina@gmail.com>
2024-03-21 14:50:50 +0200

    1.Start

---
+++ project1/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProviderComponent } from './provider/provider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ProviderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ project1/src/app/provider/provider.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider',
+  templateUrl: './provider.component.html',
+  styleUrl: './provider.component.css'
+})
+export class ProviderComponent {
+  names: string[];
+  constructor() {
+  this.names = [' Матрица ', ' Elektron ', ' Sofportal ', ' VELLOPT '];
+  }
+}
+ 
